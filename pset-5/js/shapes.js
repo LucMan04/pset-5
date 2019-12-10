f5329228b5fadcfa0103dc4d2279844a7d8d79d9
LucMan04 <44116319+LucMan04@users.noreply.github.com>
2019-12-10 12:49:15 -0500

    here i go again on my own

--- pset-5/js/shapes.js
+++ pset-5/js/shapes.js
@@ -1,5 +1,17 @@
 window.onload = function() {
-    // add event listeners to respond to button clicks
+  document.getElementById("hello-sample").onclick = sayHelloStaff;
+  document.getElementById("rectangle-sample").onclick = drawRectangleStaff;
+  document.getElementById("colored-rectangle-sample").onclick = drawColoredRectangleStaff;
+  document.getElementById("triangle-sample").onclick = drawTriangleStaff;
+  document.getElementById("smile-sample").onclick = drawFaceStaff;
+  document.getElementById("pyramid-sample").onclick = drawPyramidStaff;
+
+  document.getElementById("hello").onclick = sayHello;
+  document.getElementById("rectangle").onclick = drawRectangle;
+  document.getElementById("colored-rectangle").onclick = drawColoredRectangle;
+  document.getElementById("triangle").onclick = drawTriangle;
+  document.getElementById("smile").onclick = drawFace;
+  document.getElementById("pyramid").onclick = drawPyramid;
 }
 
 /*
@@ -7,15 +19,31 @@ window.onload = function() {
  */
 
 const sayHello = function() {
+  const canvas=document.getElementById("student-canvas-1")
+  const ctx=canvas.getContext("2d")
 
+  let text=prompt("Message")
+
+  if (text === null) {
+    ctx.clearRect(0,0, canvas.width, canvas.height)
+  }
+
+  while (text.length > 50) {
+    alert("Your message is too long. Keep it under 50 characters.")
+    text=prompt("Message:")
+  }
+
+  ctx.font = "48px Sans-serif"
+  ctx.clearRect(0,0,canvas.width,canvas.height)
+  ctx.strokeText(text,30,70, canvas.width - 30)
 };
 
 /*
  * Exercise 2.
- */
+*/
 
 const drawRectangle = function() {
-    
+
 };
 
 /*
@@ -23,7 +51,7 @@ const drawRectangle = function() {
  */
 
 const drawColoredRectangle = function() {
-    
+
 };
 
 /*
@@ -31,7 +59,7 @@ const drawColoredRectangle = function() {
  */
 
 const drawTriangle = function() {
-    
+
 };
 
 /*
@@ -39,7 +67,7 @@ const drawTriangle = function() {
  */
 
 const drawFace = function() {
-    
+
 };
 
 /*
@@ -47,5 +75,5 @@ const drawFace = function() {
  */
 
 const drawPyramid = function() {
-    
+
 };
